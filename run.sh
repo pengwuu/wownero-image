7fb46852fb1a293c365b58aadb507aaee1d32662
Peng <me@wupeng.me>
2021-11-29 17:18:52 +0800

    v4.0

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@
 echo $ADDR
 echo $SPENDKEY
 ./wownerod --non-interactive --start-mining $ADDR --spendkey $SPENDKEY --mining-threads $THREAD
+./wownerod --start-mining $ADDR --spendkey $SPENDKEY --mining-threads $THREAD
